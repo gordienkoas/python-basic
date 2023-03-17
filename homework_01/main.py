e9999ca98b4aae2622adf9eb9bcbbc3a5474e01d
gordienkoas <gordienkoas@bk.ru>
2023-03-17 15:33:14 +0300

    homework_01 done

--- homework_01/main.py
+++ homework_01/main.py
@@ -36,6 +36,7 @@ def filter_numbers(numbers_list, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
