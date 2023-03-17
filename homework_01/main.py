21a1308aa8105e24893c050a3496b95e5e241789
gordienkoas <gordienkoas@bk.ru>
2023-03-17 15:30:25 +0300

    homework_01 done

--- homework_01/main.py
+++ homework_01/main.py
@@ -19,7 +19,13 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(n):
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n
+
+def filter_numbers(numbers_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +36,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return [number for number in numbers_list if is_prime(number) == True]
