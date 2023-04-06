dc41581c443100154f4067d4638a891d49bf19a5
gordienkoas <127838071+gordienkoas@users.noreply.github.com>
2023-04-07 00:13:57 +0300

    plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -6,9 +6,29 @@ from exceptions import  CargoOverload
 
 class Plane(Vehicle):
 
-    def __init__(self, weight:int, fuel:int, fuel_consumption:int, cargo=0, max_cargo=0):
-        self.cargo = cargo
+    def __init__(self,
+                 weight:int,
+                 fuel:int,
+                 fuel_consumption:int,
+                 max_cargo=int):
+        super().__init__()
+        self.cargo = 0
         self.max_cargo = max_cargo
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+        self.weight = weight
+
+        def load_cargo(self, added_load: int):
+            if self.cargo + added_load <= self.max_cargo:
+                self.cargo += added_load
+            else:
+                raise CargoOverload
+
+        def remove_all_cargo(self) -> int:
+            result = self.cargo
+            self.cargo = 0
+            return result
 
 
-print(Plane.__dict__)
+
+
