f7233f0f04616d5b049d37482a66f50e4c31afe3
gordienkoas <gordienkoas@bk.ru>
2023-04-07 10:10:17 +0300

    homework_02 done

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,21 +1,21 @@
 from abc import ABC
-from exceptions import LowFuelError, NotEnoughFuel
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
     weight: int = 0
 
-    def __init__(self, weight=0, fuel=0, fuel_consumption=3):
+    def __init__(self, weight=0, fuel=40, fuel_consumption=3):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
         self.started = False
-
     def start(self):
         if not self.started:
             if self.fuel < self.fuel_consumption:
                 raise LowFuelError
-
+            else:
+                self.started = True
     def move(self, distance: int):
         required_fuel = distance * (self.fuel_consumption)
         fuel_now = self.fuel
--- homework_02/car.py
+++ homework_02/car.py
@@ -1,15 +1,15 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-from base import Vehicle
-from engine import Engine
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
 
 class Car(Vehicle):
     def __init__(self, weight=int, fuel=int, fuel_consumption=int, *args, **kwargs):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
-        self.engine = engine
+        self.engine = Engine
 
     def set_engine(self, engine: Engine):
         if isinstance(engine, Engine):
