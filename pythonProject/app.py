e546cc357feb61e2b123410d0c2d4e70a08a109a
user <gordienkoas@bk.ru>
2023-07-03 00:29:04 +0300

    add python project

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,16 +1,16 @@
-import sqlite3
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///homework_06/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -33,7 +33,7 @@ def create_add():
         try:
             db.session.add(user)
             db.session.commit()
-            return redirect('/')
+            return render_template('done.html')
         except:
             return 'При добавлении произошла ошибка'
     else:
@@ -41,7 +41,9 @@ def create_add():
 
 @app.route("/viewpage/", endpoint="viewpage")
 def create_add():
-    return render_template("viewpage.html")
+    users = User.query.all()
+    return render_template("viewpage.html", users=users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
