022eb35f118e3ad39996448609418faad0a603a7
user <gordienkoas@bk.ru>
2023-11-06 22:19:46 +0300

    homework_08 done (remake admin)

--- homework_08/sales_shop/shop/urls.py
+++ homework_08/sales_shop/shop/urls.py
@@ -2,6 +2,7 @@ from django.template.defaulttags import url
 from django.urls import path, include
 #from .views import shop_index, categories_with_products_three
 import shop.views as shop
+from shop import admin
 
 app_name = "shop"
 
